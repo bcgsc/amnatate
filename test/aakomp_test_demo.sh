cc8cc52fb76657797c425d84af0a77a1c5b90b0a
jowong04 <jowong@hpce705.hpc.bcgsc.ca>
2025-05-15 11:17:24 -0700

    update test

--- test/aakomp_test_demo.sh
+++ test/aakomp_test_demo.sh
@@ -2,15 +2,12 @@
 
 set -eu -o pipefail
 
-echo "Decompressing genome..."
-gunzip -c GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz > GCA_009914755.4_T2T-CHM13v2.0_genomic.fna
-
 echo "Extracting primates_odb12.tar.gz..."
 tar -xzf primates_odb12.tar.gz
 
 # Run aaKomp
 echo "Running aaKomp..."
-run-aakomp --db-dir ./ --reference primates_odb12/primates_odb12.faa --input GCA_009914755.4_T2T-CHM13v2.0_genomic.fna -o demo
+run-aakomp --db-dir ./ --reference primates_odb12/primates_odb12.faa --input small_test.fa -t 4  -o demo
 
 exit 0
 
