61cef182e5ae19d5eb57a95bc4a2b89c7d486b2e
JW <34543031+jwcodee@users.noreply.github.com>
2025-05-15 09:26:20 -0700

    Update aakomp_test_demo.sh

--- test/aakomp_test_demo.sh
+++ test/aakomp_test_demo.sh
@@ -2,30 +2,24 @@
 
 set -eu -o pipefail
 
-# Download the C. elegans genome
-echo "Downloading C. elegans genome..."
-wget -nc https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/985/GCF_000002985.6_WBcel235/GCF_000002985.6_WBcel235_genomic.fna.gz
+# Download the H. sapiens genome
+echo "Downloading H. sapiens genome..."
+wget -nc https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/914/755/GCA_009914755.4_T2T-CHM13v2.0/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz
 
 # Decompress the genome file
 echo "Decompressing genome..."
-gunzip -c GCF_000002985.6_WBcel235_genomic.fna.gz > GCF_000002985.6_WBcel235_genomic.fna
+gunzip -c GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz > GCA_009914755.4_T2T-CHM13v2.0_genomic.fna
 
-# Link the protein reference file
-echo "Linking nematoda_odb12.faa..."
-ln -sf nematoda_odb12/nematoda_odb12.faa ./
+# Download and extract primates_odb12.tar.gz
+echo "Downloading primates_odb12.tar.gz..."
+wget -nc https://zenodo.org/records/15421506/files/primates_odb12.tar.gz
+
+echo "Extracting primates_odb12.tar.gz..."
+tar -xzf primates_odb12.tar.gz
 
 # Run aaKomp
 echo "Running aaKomp..."
-run-aakomp --db-dir ./ --reference nematoda_odb12.faa --input GCF_000002985.6_WBcel235_genomic.fna -o demo
-
-# Check GFF comparison
-echo "Checking for differences from expected output..."
-if comm -23 test.gff demo.gff | grep .; then
-    echo -e "\nTest failed - output differs from test.gff"
-    exit 1
-else
-    echo -e "\nTest successful - outputs match!"
-fi
+run-aakomp --db-dir ./ --reference primates_odb12/primates_odb12.faa --input GCA_009914755.4_T2T-CHM13v2.0_genomic.fna -o demo
 
 exit 0
 
