286de2bc2626375f1b432854a79dc4fd350e4411
JW <34543031+jwcodee@users.noreply.github.com>
2025-05-15 18:26:45 -0700

    Update aakomp_test_demo.sh

--- test/aakomp_test_demo.sh
+++ test/aakomp_test_demo.sh
@@ -2,12 +2,25 @@
 
 set -eu -o pipefail
 
+expected_score="2.00544"
+
 echo "Extracting primates_odb12.tar.gz..."
 tar -xzf primates_odb12.tar.gz
 
 # Run aaKomp
 echo "Running aaKomp..."
-run-aakomp --db-dir ./ --reference primates_odb12/primates_odb12.faa --input small_test.fa -t 4  -o demo
+run-aakomp --db-dir ./ --reference primates_odb12/primates_odb12.faa --input small_test.fa -t 4 -o demo
+
+# Compare score
+score=$(head -n 1 demo_score.txt)
+echo "Observed score: $score"
+echo "Expected score: $expected_score"
+
+if (( $(echo "$score == $expected_score" | bc -l) )); then
+  echo "Score matches expected value."
+else
+  echo "Score does not match expected value."
+  exit 1
+fi
 
 exit 0
-
