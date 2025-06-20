d5bbab0cc1603635d4ace4dba538b5bb8a3be503
jowong04 <jowong@hpce705.hpc.bcgsc.ca>
2025-06-20 09:40:06 -0700

    make_mibf: remove sequences shorter than k

--- make_mibf.cpp
+++ make_mibf.cpp
@@ -40,6 +40,7 @@ make_small_mibf(const std::string &seq, size_t hash_num, size_t kmer_size) {
       calc_optimal_size(filter_size, hash_num, TARGET_FALSE_POSITIVE_RATE),
       hash_num);
 
+
   for (size_t stage = 0; stage < STAGES; ++stage) {
     btllib::AAHash itr(seq, hash_num, kmer_size, 1);
     btllib::AAHash itr2(seq, hash_num, kmer_size, 2);
@@ -200,6 +201,9 @@ int main(int argc, char *argv[]) {
     btllib::SeqReader reader(reference_path,
                              btllib::SeqReader::Flag::LONG_MODE);
     for (const auto &record : reader) {
+      if (record.seq.size() < (size_t)kmer_size + 5){
+          continue;
+      }
       seq_ID_to_miBf_ID[record.id] = miBf_ID;
       miBf_ID_to_seq_ID_and_len[miBf_ID] = {record.id, record.seq.size()};
       miBf_ID_to_seq[miBf_ID] = record.seq;
@@ -221,6 +225,9 @@ int main(int argc, char *argv[]) {
                              btllib::SeqReader::Flag::LONG_MODE);
 #pragma omp parallel
     for (const auto &record : reader) {
+      if (record.seq.size() < (size_t)kmer_size + 5){
+          continue;
+      }
       btllib::AAHash itr(record.seq, hash_num, kmer_size, 1);
       btllib::AAHash itr2(record.seq, hash_num, kmer_size, 2);
       btllib::AAHash itr3(record.seq, hash_num, kmer_size, 3);
@@ -261,6 +268,9 @@ int main(int argc, char *argv[]) {
   btllib::SeqReader reader(reference_path, btllib::SeqReader::Flag::LONG_MODE);
 #pragma omp parallel
   for (const auto &record : reader) {
+      if (record.seq.size() < (size_t)kmer_size + 5){
+          continue;
+      }
     auto mi_bf_small = make_small_mibf(record.seq, hash_num, rescue_kmer_size);
     const auto &miBf_ID = seq_ID_to_miBf_ID[record.id];
     mi_bf_small.save(output_prefix + "." + std::to_string(miBf_ID) + ".mibf");
